1ac3a69b1a6feea554b68d64c693efcf37a7896d
rafael0117 <mamanirafael324@gmail.com>
2025-08-04 14:49:48 -0500

    Header and Footer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import ListClientesComponent from './components/ListClientesComponent';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ListClientesComponent/>
     </div>
   );
 }
